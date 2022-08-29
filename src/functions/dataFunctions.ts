fe5c2013233ad6168d4a837e5c39068f4fa6bf8c
saintabomination <saint.abomination@protonmail.com>
2022-08-29 16:19:51 +0200

    Basic reminder fetch function

---
+++ src/functions/dataFunctions.ts
@@ -0,0 +1,9 @@
+export const fetchReminders = () => {
+  return [
+    { id: 0, text: 'Reminder 1', date: '11. 3.' },
+    { id: 1, text: 'Reminder 2', date: '12. 3.' },
+    { id: 2, text: 'Reminder 3', date: '13. 3.' },
+    { id: 3, text: 'Reminder 4', date: '14. 3.' },
+    { id: 4, text: 'Reminder 5', date: '15. 3.' },
+  ];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+import { fetchReminders } from './functions/dataFunctions';
+
+const reminders = fetchReminders();
+reminders.forEach(reminder => {
+  console.log(reminder.text);
+});
