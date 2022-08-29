1394bd706f54165760043088bf6b2eb7b3f12f0b
saintabomination <saint.abomination@protonmail.com>
2022-08-29 16:36:27 +0200

    Adding typings for reminders

--- src/functions/dataFunctions.ts
+++ src/functions/dataFunctions.ts
@@ -1,9 +1,11 @@
-export const fetchReminders = () => {
+import type { Reminder } from '../typings/entriesTypes'; 
+
+export const fetchReminders = (): Reminder[] => {
   return [
-    { id: 0, text: 'Reminder 1', date: '11. 3.' },
-    { id: 1, text: 'Reminder 2', date: '12. 3.' },
-    { id: 2, text: 'Reminder 3', date: '13. 3.' },
-    { id: 3, text: 'Reminder 4', date: '14. 3.' },
-    { id: 4, text: 'Reminder 5', date: '15. 3.' },
+    { id: '0', text: 'Reminder 1', date: '11. 3.' },
+    { id: '1', text: 'Reminder 2', date: '12. 3.' },
+    { id: '2', text: 'Reminder 3', date: '13. 3.' },
+    { id: '3', text: 'Reminder 4', date: '14. 3.' },
+    { id: '4', text: 'Reminder 5', date: '15. 3.' },
   ];
 }
---
+++ src/typings/entriesTypes.ts
@@ -0,0 +1,5 @@
+export type Reminder = {
+  id: string;
+  text: string;
+  date: string;
+};
